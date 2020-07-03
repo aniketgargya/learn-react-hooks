ee14551334f2d2ca0f61393f6a89af7e8895aa48
Aniket Gargya <gargya.aniket@gmail.com>
2020-07-03 00:13:52 -0500

    created good project structure

---
+++ useState/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <>
+            <h1>React App!</h1>
+            <p>Welcome to a Webpack Babel React App</p>
+        </>
+    );
+};
+
+export default App;
---
+++ useState/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: path.join(__dirname, "src", "index.js"),
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "index.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "index.html")
+        })
+    ]
+};
